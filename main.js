62b0e17cfa465deb4bd7ee83bdf3572d8303e946
baran-yldz <baran.yildiz@yahoo.com>
2026-03-07 22:48:42 +0300

    Header

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const mobileToggle = document.getElementById('mobile-toggle');
-    const navMenu = document.getElementById('nav-menu');
+    const navLinks = document.querySelectorAll('.nav-link');
+    const menuCollapse = document.getElementById('socialishaNav');
+    
+    // Bootstrap Collapse objesini başlat
+    const bsCollapse = new bootstrap.Collapse(menuCollapse, {toggle: false});
 
-    mobileToggle.addEventListener('click', () => {
-        // Menüyü aç/kapat
-        navMenu.classList.toggle('active');
-        
-        // Hamburger ikonunu "X" formuna sokmak istersen buraya animasyon ekleyebiliriz
-        console.log("Menü tetiklendi");
-    });
-
-    // Menü linklerinden birine tıklandığında menüyü otomatik kapat
-    const navLinks = document.querySelectorAll('.nav-left ul li a');
     navLinks.forEach(link => {
         link.addEventListener('click', () => {
-            navMenu.classList.remove('active');
+            if (window.innerWidth < 992) {
+                bsCollapse.hide();
+            }
         });
     });
 });
